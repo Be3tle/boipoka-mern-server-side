f48519a91b4994f39905e0c0996469179662a18b
Be3tle <aburatinshoaib@gmail.com>
2023-11-06 11:43:12 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
