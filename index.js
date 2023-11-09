d36f90d40084aaf63e64c682732e76b875b4017b
Be3tle <aburatinshoaib@gmail.com>
2023-11-09 16:05:11 +0600

    jwt api

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,13 +12,14 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      // 'https://cars-doctor-6c129.web.app',
-      // 'https://cars-doctor-6c129.firebaseapp.com'
+      'https://boip0ka.web.app',
+      'https://boip0ka.firebaseapp.com',
     ],
     credentials: true,
   })
 );
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.bvbzn4c.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -29,11 +32,59 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middleware
+
+const logger = async (req, res, next) => {
+  console.log('called', req.hostname, req.originalUrl);
+  next();
+};
+
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log('Value of token in middleware', token);
+  if (!token) {
+    return res.status(401).send({ message: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    //error
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: 'Unauthorized' });
+    }
+    // decoded if token is valid
+    console.log('Value in the token', decoded);
+    req.user = decoded;
+    next();
+  });
+};
+
 async function run() {
   try {
     const genreCollection = client.db('boipoka').collection('genre');
     const bookCollection = client.db('boipoka').collection('books');
     const borrowCollection = client.db('boipoka').collection('borrows');
+
+    app.post('/jwt', logger, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: '1h',
+      });
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none',
+        })
+        .send({ success: true });
+    });
+
+    app.post('/logout', async (req, res) => {
+      const user = req.body;
+      console.log('logging out', user);
+      res.clearCookie('token', { maxAge: 0 }).send({ success: true });
+    });
 
     app.get('/genre', async (req, res) => {
       const cursor = genreCollection.find();
@@ -41,7 +92,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get('/books', async (req, res) => {
+    app.get('/books', logger, async (req, res) => {
       const cursor = bookCollection.find();
       const result = await cursor.toArray();
       res.send(result);
@@ -74,6 +125,7 @@ async function run() {
           genre: updatedBook.genre,
           imgUrl: updatedBook.imgUrl,
           rating: updatedBook.rating,
+          quantity: updatedBook.quantity,
         },
       };
 
@@ -85,7 +137,14 @@ async function run() {
 
     // borrows
 
-    app.get('/borrows', async (req, res) => {
+    app.get('/borrows', logger, verifyToken, async (req, res) => {
+      console.log(req.query.email);
+      // console.log('token', req.cookies.token);
+      console.log('User in the valid token:', req.user);
+
+      if (req.query.email !== req.user.email) {
+        return res.status(403).send({ message: 'Forbidden access' });
+      }
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
